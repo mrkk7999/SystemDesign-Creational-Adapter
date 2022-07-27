c0dd0ee96822a78df5c18829a054ee77ac3465e7
mrkk7999 <82576116+mrkk7999@users.noreply.github.com>
2022-07-27 23:29:06 +0530

    Added required files

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+func main() {
+	client := &Client{}
+	mac := &Mac{}
+
+	client.InsertLightningConnectorIntoComputer(mac)
+
+	windowsMachine := &Windows{}
+
+	windowsMachineAdapter := &windowsAdapter{windowMachine: windowsMachine}
+
+	client.InsertLightningConnectorIntoComputer(windowsMachineAdapter)
+
+}
